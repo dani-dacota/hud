79acfab8274ebd2e9ba0b83502003d20d41cbcb4
dani-dacota <daniyial.siddiqi@gmail.com>
2019-11-29 07:25:56 -0800

    add delay

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import imu
-import os
+import time
 
 imu = imu.IMU()
 
 while True:
     # print('Distance:', IMU.dist())
     print('Gyro X:', imu.gyro_x(), ', Y:', imu.gyro_y(), ', Z:', imu.gyro_z())
+    time.sleep(0.5)
