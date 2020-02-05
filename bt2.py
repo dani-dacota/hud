e9633fa4ae2b44b86d27a7673c60d230fca6f9f6
dani-dacota <daniyial.siddiqi@gmail.com>
2020-02-04 19:22:31 -0800

    bt2

---
+++ bt2.py
@@ -0,0 +1,15 @@
+import Bluetooth
+
+server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+port = 1
+server_socket.bind(("",port))
+server_socket.listen(1)
+
+client_socket,address = server_socket.accept()
+print ("Accepted connection from ",address)
+while True:
+    data = client_socket.recv(1024)
+    print ("Received:", str(data))
+
+client_socket.close()
+server_socket.close()
