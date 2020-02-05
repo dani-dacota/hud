90cd6ba1f6c63405992c7de628a12267d3a45016
dani-dacota <daniyial.siddiqi@gmail.com>
2020-02-04 18:54:40 -0800

    bt oled

--- bt.py
+++ bt.py
@@ -4,22 +4,59 @@ Bluetooth using Python sockets (with Python 3.3 or above).
 """
 
 import socket
+import qwiic
 
-hostMACAddress = 'B8:27:EB:3A:A3:33' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
-port = 3 # 3 is an arbitrary choice. However, it must match the port used by the client.
-backlog = 1
-size = 1024
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.bind((hostMACAddress,port))
-s.listen(backlog)
-try:
-    client, address = s.accept()
-    while 1:
-        data = client.recv(size)
-        if data:
-            print(data)
-            client.send(data)
-except:	
-    print("Closing socket")	
-    client.close()
-    s.close()
+def display(x, y, message):
+    oled.clear(oled.PAGE)
+    oled.set_cursor(x, y)
+    oled.print(message)
+    oled.display()
+
+
+
+if __name__ == "__main__":
+    # Define oled screen and initialize
+    oled = qwiic.QwiicMicroOled()
+    oled.begin()
+
+    # clear the screen
+    oled.clear(oled.PAGE)
+    oled.display()
+
+    # set the font size
+    oled.set_font_type(1)
+
+    print("Waiting to receive BT data")
+
+    # The MAC address of a Bluetooth adapter on the server
+    hostMACAddress = 'B8:27:EB:3A:A3:33' 
+
+    # 3 is arbitrary but must match the port used by the client
+    port = 3 
+
+    # similar to queue depth
+    backlog = 1
+
+    #block size in bytes
+    size = 1024
+
+    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    s.bind((hostMACAddress,port))
+    s.listen(backlog)
+
+    # set cursor position
+    display(5,5,'BT')
+
+    try:
+        client, address = s.accept()
+        while True:
+            data = client.recv(size)
+            if data:
+                print(data)
+                display(5,5, str(data))
+                client.send(data)
+    except:	
+        print('Closing socket')	
+        display(5,5, 'Error')
+        client.close()
+        s.close()
