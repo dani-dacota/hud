141d23a95e99493d7d6fa04f52ba94e2a5749a10
Muhammad Siddiqi <muhammad.siddiqi@wdc.com>
2020-02-10 08:56:13 -0800

    add countdown.py

---
+++ countdown.py
@@ -0,0 +1,24 @@
+import qwiic
+import time
+
+# Define oled screen and initialize
+oled = qwiic.QwiicMicroOled()
+oled.begin()
+    
+def oled_print(message):
+    # clear the screen
+    oled.clear(oled.PAGE)
+    oled.display()
+
+    # set the font size
+    oled.set_font_type(1)
+
+    oled.set_cursor(2, 5) 
+    oled.print(message)
+
+    # display screen
+    oled.display()
+
+for i in range(30):
+    oled_print(str(30-i))
+    time.sleep(1)
